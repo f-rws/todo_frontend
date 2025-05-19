770d1d5e2462d5cd3fbd65cdfb1dd1494fa02cef
Hiroya Furusawa <pogbba.6@gmail.com>
2025-05-20 01:23:25 +0900

    refactor: typo修正

--- src/features/tasks/schemas/index.ts
+++ src/features/tasks/schemas/index.ts
@@ -1,7 +1,7 @@
 import { z } from 'zod';
 import type { Task } from '../types';
 
-export const taskShema = z.object({
+export const taskSchema = z.object({
   id: z.string(),
   title: z.string().min(1).max(50),
   description: z.string().max(1000),
