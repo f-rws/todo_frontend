c7e56ec126bf635c359a0e678d08885e9d3660f2
Hiroya Furusawa <pogbba.6@gmail.com>
2025-05-22 08:17:06 +0900

    feat: taskSchemaDescription, taskSchemaStatus, taskSchemaCreatedAt, taskSchemaUpdatedAt, taskSchemaExpiredAtの単体テストを追加

--- src/features/tasks/schemas/__tests__/tasksSchemas.test.ts
+++ src/features/tasks/schemas/__tests__/tasksSchemas.test.ts
@@ -1,5 +1,13 @@
 import { describe, expect, test } from 'vitest';
-import { taskSchemaId, taskSchemaTitle } from '..';
+import {
+  taskSchemaId,
+  taskSchemaTitle,
+  taskSchemaDescription,
+  taskSchemaStatus,
+  taskSchemaCreatedAt,
+  taskSchemaUpdatedAt,
+  taskSchemaExpiredAt,
+} from '..';
 import type { Task } from '../../types';
 
 const validTask = {
@@ -41,5 +49,89 @@ describe.concurrent('taskSchemaTitleのテスト', () => {
 
   test.for(cases)('$description $value -> $expected', ({ value, expected }) => {
     expect(taskSchemaTitle.safeParse(value).success).toBe(expected);
+  });
+});
+
+describe.concurrent('taskSchemaDescriptionのテスト', () => {
+  const cases: TestCase[] = [
+    { value: validTask.description, expected: true, description: '有効な値' },
+    { value: '', expected: true, description: '空文字を許容' },
+    { value: 'a'.repeat(1000), expected: true, description: '最長1000文字' },
+    { value: 'a'.repeat(1001), expected: false, description: '1001文字以上は許容しない' },
+  ];
+  test.for(cases)('$description $value -> $expected', ({ value, expected }) => {
+    expect(taskSchemaDescription.safeParse(value).success).toBe(expected);
+  });
+});
+
+describe.concurrent('taskSchemaStatusのテスト', () => {
+  const cases: TestCase[] = [
+    { value: 'notStarted', expected: true, description: 'notStartedは有効な値' },
+    { value: 'inProgress', expected: true, description: 'inProgressは有効な値' },
+    { value: 'completed', expected: true, description: 'completedは有効な値' },
+    { value: '', expected: false, description: '空文字は許容しない' },
+  ];
+
+  test.for(cases)('$description $value -> $expected', ({ value, expected }) => {
+    expect(taskSchemaStatus.safeParse(value).success).toBe(expected);
+  });
+});
+
+describe.concurrent('taskSchemaCreatedAtのテスト', () => {
+  const cases: TestCase[] = [
+    { value: validTask.createdAt, expected: true, description: 'UTC形式は有効な値' },
+    {
+      value: '2024-03-20T00:00:00+09:00',
+      expected: false,
+      description: 'タイムゾーン付きは許容しない',
+    },
+    { value: '2024-03-20', expected: false, description: '日付のみは許容しない' },
+    {
+      value: '2024/03/20T00:00:00+09:00',
+      expected: false,
+      description: '区切り線がスラッシュは許容しない',
+    },
+    { value: '', expected: false, description: '空文字は許容しない' },
+  ];
+
+  test.for(cases)('$description $value -> $expected', ({ value, expected }) => {
+    expect(taskSchemaCreatedAt.safeParse(value).success).toBe(expected);
+  });
+});
+
+describe.concurrent('taskSchemaUpdatedAtのテスト', () => {
+  const cases: TestCase[] = [
+    { value: validTask.updatedAt, expected: true, description: 'UTC形式は有効な値' },
+    { value: null, expected: true, description: 'nullは許容' },
+    {
+      value: '2024-03-20T00:00:00+09:00',
+      expected: false,
+      description: 'タイムゾーン付きは許容しない',
+    },
+    { value: '2024-03-20', expected: false, description: '日付のみは許容しない' },
+    { value: '2024/03/20', expected: false, description: '区切り線がスラッシュは許容しない' },
+    { value: '', expected: false, description: '空文字は許容しない' },
+  ];
+
+  test.for(cases)('$description $value -> $expected', ({ value, expected }) => {
+    expect(taskSchemaUpdatedAt.safeParse(value).success).toBe(expected);
+  });
+});
+
+describe.concurrent('taskSchemaExpiredAtのテスト', () => {
+  const cases: TestCase[] = [
+    { value: validTask.expiredAt, expected: true, description: 'UTC形式は有効な値' },
+    {
+      value: '2024-03-20T00:00:00+09:00',
+      expected: false,
+      description: 'タイムゾーン付きは許容しない',
+    },
+    { value: '2024-03-20T00:00:00Z', expected: false, description: '時間込みは許容しない' },
+    { value: '2024/03/20', expected: false, description: '区切り線がスラッシュは許容しない' },
+    { value: '', expected: false, description: '空文字は許容しない' },
+  ];
+
+  test.for(cases)('$description $value -> $expected', ({ value, expected }) => {
+    expect(taskSchemaExpiredAt.safeParse(value).success).toBe(expected);
   });
 });
--- src/features/tasks/schemas/index.ts
+++ src/features/tasks/schemas/index.ts
@@ -3,13 +3,18 @@ import type { Task } from '../types';
 
 export const taskSchemaId = z.string();
 export const taskSchemaTitle = z.string().min(1).max(50);
+export const taskSchemaDescription = z.string().max(1000);
+export const taskSchemaStatus = z.enum(['notStarted', 'inProgress', 'completed']);
+export const taskSchemaCreatedAt = z.string().datetime();
+export const taskSchemaUpdatedAt = z.union([z.string().datetime(), z.null()]);
+export const taskSchemaExpiredAt = z.string().date();
 
 export const taskSchema = z.object({
   id: taskSchemaId,
   title: taskSchemaTitle,
-  description: z.string().max(1000),
-  status: z.enum(['notStarted', 'inProgress', 'completed']),
-  createdAt: z.string().datetime(),
-  updatedAt: z.union([z.string().datetime(), z.null()]),
-  expiredAt: z.string().date(),
+  description: taskSchemaDescription,
+  status: taskSchemaStatus,
+  createdAt: taskSchemaCreatedAt,
+  updatedAt: taskSchemaUpdatedAt,
+  expiredAt: taskSchemaExpiredAt,
 }) satisfies z.ZodSchema<Task>;
