74a8cfcadda125db70338985d353aaffee098752
Hiroya Furusawa <pogbba.6@gmail.com>
2025-06-13 01:14:55 +0900

    test: apiClent.deleteのテストを追加

--- src/lib/api-client/__tests__/index.test.ts
+++ src/lib/api-client/__tests__/index.test.ts
@@ -100,7 +100,40 @@ describe('apiClientのテスト', () => {
       };
       vi.spyOn(axiosInstance, 'put').mockRejectedValue(mockAxiosError);
 
-      const response = await apiClient.put('/tasks/', mockRequestData);
+      const response = await apiClient.put('/tasks/1/', mockRequestData);
+
+      expect(response.data).toBeUndefined();
+      expect(response.error).toEqual({
+        status: mockStatus,
+        message: mockMessage,
+        error: mockAxiosError,
+      });
+    });
+  });
+
+  describe('delete', () => {
+    it('正常系', async () => {
+      vi.spyOn(axiosInstance, 'delete').mockResolvedValue({ data: null });
+
+      const response = await apiClient.delete('/tasks/');
+
+      expect(response.data).toBe(null);
+      expect(response.error).toBeUndefined();
+    });
+
+    it('異常系', async () => {
+      const mockStatus = 500;
+      const mockMessage = 'Internal Server Error';
+      const mockAxiosError: MockAxiosError = {
+        isAxiosError: true,
+        response: {
+          status: mockStatus,
+        },
+        message: mockMessage,
+      };
+      vi.spyOn(axiosInstance, 'delete').mockRejectedValue(mockAxiosError);
+
+      const response = await apiClient.delete('/tasks/1/');
 
       expect(response.data).toBeUndefined();
       expect(response.error).toEqual({
